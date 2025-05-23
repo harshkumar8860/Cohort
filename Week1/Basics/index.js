922cc682bd0acb78047de40ff7c3420d8eda41ee
Harsh Kumar <harshkumar8860@gmail.com>
2025-05-23 22:28:23 +0530

    some basic of js

---
+++ Week1/Basics/index.js
@@ -0,0 +1,61 @@
+// // let a = "10";
+// // let b = 20;
+// // let c = a + b;
+
+const { log } = require("console");
+
+
+// // console.log(c);
+// // console.log(typeof(c));
+
+// // create a fn that takes an array of objects as input, and returns the users who are older than 18 and are male
+
+
+// const users = [{
+//   name: "John", 
+//   age: 21,
+//   gender:"male"
+// },{
+//   name: "Jane",
+//   age: 31,
+//   gender:"female"
+// },{
+//   name: "Peter",
+//   age: 15,
+//   gender:"male"},]
+
+// function loop(arr){
+//   let arr2 = [];
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i].age >18 && arr[i].gender ==="male")            {
+//         arr2.push(arr[i]);
+//       }
+//   }
+//   return arr2;
+// }
+
+// function map(arr){
+//   let arr2 = arr.map(function(element){
+//     if(element.age > 18 && element.gender === "male"){
+//       return element;
+//     }
+//   })
+//   let arr3 = arr2.filter(function(element){
+//     return element !== undefined;
+//   })
+//   return arr3;
+// }
+
+// function myfn(arr){
+//  let arr2 = arr.filter(function(element){
+//   return element.age > 18 && element.gender === "male";
+//  })  
+//   return arr2;
+// }
+
+
+
+
+// console.log(loop(users))
+// console.log(map(users))
+// console.log(myfn(users));
