51887330bf9b24d611cbaaea521473aad116435d
Harsh Kumar <harshkumar8860@gmail.com>
2025-07-02 10:46:23 +0530

    some basics of nodejs

---
+++ week4/introToNode/index.js
@@ -0,0 +1,5 @@
+const path = require("path");
+console.log(__dirname);
+console.log(__dirname + "\index.js");
+console.log(__dirname + "..\..\index.js" + "projects" + "abc.txt");
+console.log(path.join(__dirname , "../../index.js" , "projects" , "abc.txt"));
